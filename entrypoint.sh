9bde713aedfc04a80537ce7183e144c4c76e9a16
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-12-03 17:14:50 +0000

    make omp_num_threads importable

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ TOTAL_CORES=`expr ${CORES:-1} \* ${SOCKETS:-1}`
 KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
 KMP_BLOCKTIME=1
 
-export OMP_NUM_THREADS=$TOTAL_CORES
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:-$TOTAL_CORES}
 export $KMP_SETTING
 export KMP_BLOCKTIME=$KMP_BLOCKTIME
 
-echo -e "### using OMP_NUM_THREADS=$TOTAL_CORES"
+echo -e "### using OMP_NUM_THREADS=$OMP_NUM_THREADS"
 echo -e "### using $KMP_SETTING"
 echo -e "### using KMP_BLOCKTIME=$KMP_BLOCKTIME\n"
 
